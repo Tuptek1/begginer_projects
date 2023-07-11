367ccb30a910b7fa3059b287a2efad1e9b518150
Tuptek1 <tuptek112@gmail.com>
2023-07-11 18:48:10 +0200

    finished testing and added tic tac toe file

--- tkinter_lib_test.py
+++ tkinter_lib_test.py
@@ -1,11 +1,63 @@
 from tkinter import *
-from tkinter import ttk
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=2)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=2, row=4)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=3, row=6)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=4, row=8)
+import tkinter as tk
+
+
+
+def check(var, index, mode):
+    if chbox_var.get() == 1:
+        print("The checkbox is checked")
+    else:
+        print("The checkbox is not checked")
+
+def submit():
+    
+    name=name_var.get()
+    password = passw_var.get()
+        
+    print("The name is : " + name)
+    print("The password is : " + password)
+        
+    name_var.set("")
+    passw_var.set("")
+
+root = Tk() 
+root.geometry('400x150')
+root.title("Username")
+
+menu_bar = Menu(root)
+menu_1 = Menu(menu_bar, tearoff = 0)
+
+menu_bar.add_cascade(label = "Menu1", menu = menu_1)
+menu_1.add_command(label = 'Nigger')
+menu_1.add_separator()
+menu_1.add_command(label = 'Quit', command= root.quit)
+
+root.config(menu=menu_bar)
+
+name_var = tk.StringVar()
+passw_var = tk.StringVar()
+chbox_var = tk.IntVar()
+
+    
+usernameLabel = Label(root, text = "Enter your username")
+usernameEntry = Entry(root, textvariable = name_var)
+passwordLabel = Label(root, text = "Enter your password")
+passwordEntry = Entry(root, textvariable = passw_var, show = "*")
+
+quitButton = Button(root, text = "Quit", command = root.quit)
+
+submitButton1 = Button(root, text = "Submit", command = submit)
+
+chbox_var.trace("w", check)
+checkButton = Checkbutton(root, variable= chbox_var)
+
+usernameLabel.grid(column=0, row=0)
+usernameEntry.grid(column=1, row=0)
+passwordLabel.grid(column=0, row=1)
+passwordEntry.grid(column=1, row=1)
+
+submitButton1.grid(column=3, row=0, pady= 2)
+quitButton.grid(column=4, row=0, pady= 2)
+checkButton.grid(column=0, row=3, pady= 2)
+
 root.mainloop()
