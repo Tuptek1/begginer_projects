d4d520db6814e5272517d6c308a0feabeb679e74
Tuptek1 <tuptek112@gmail.com>
2023-07-26 15:27:35 +0200

    finished the weight calculator

--- Weight_Converter.py
+++ Weight_Converter.py
@@ -1,5 +1,21 @@
 from tkinter import *
 
+def convert():
+    gram = float(weightEntry.get())*1000
+     
+    pound = float(weightEntry.get())*2.20462
+     
+    ounce = float(weightEntry.get())*35.274
+     
+    t1.delete("1.0", END)
+    t1.insert(END,gram)
+     
+    t2.delete("1.0", END)
+    t2.insert(END,pound)
+     
+    t3.delete("1.0", END)
+    t3.insert(END,ounce)
+
 root = Tk()
 root.title("Weight Converter")
 root.geometry('450x100')
@@ -10,7 +26,7 @@ weightLabel.grid(row=0, column=0, padx=10, pady=10)
 weightEntry = Entry(root)
 weightEntry.grid(row=0, column=1, padx=10, pady=10)
 
-convertButton = Button(root, text="Convert")
+convertButton = Button(root, text="Convert", command=convert)
 convertButton.grid(row=0, column=2, padx=10)
 
 gramLabel  = Label(root, text="Gram")
@@ -22,14 +38,14 @@ poundLabel.grid(row=1, column=1, padx=10)
 ounceLabel = Label(root, text="Ounce")
 ounceLabel.grid(row=1, column=2, padx=10)
 
-gramEntry = Entry(root)
-gramEntry.grid(row=2, column=0, padx=10)
+t1 = Text(root, height = 1, width = 20)
+t1.grid(row = 2, column = 0)
 
-poundEntry = Entry(root)
-poundEntry.grid(row=2, column=1, padx=10)
+t2 = Text(root, height = 1, width = 20)
+t2.grid(row = 2, column = 1)
 
-ounceEntry = Entry(root)
-ounceEntry.grid(row=2, column=2, padx=10)
+t3 = Text(root, height = 1, width = 20)
+t3.grid(row = 2, column = 2)
 
 root.mainloop()
 
